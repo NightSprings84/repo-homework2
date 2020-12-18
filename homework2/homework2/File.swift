d0856321bcf4d340d92a7b84737ea600af519337
nightsprings84 <s.o.d.metalist@gmail.com>
2020-12-18 16:55:56 +0300

    Added-Devided

--- homework2/homework2/File.swift
+++ homework2/homework2/File.swift
@@ -10,11 +10,17 @@ import Foundation
 func evenNumber (ourNumber: Int){
     if ourNumber % 2 == 0{
         print("even number")
+func onthree (threeNumber: Int){
+    if threeNumber % 3 == 0{
+        print("without remainder")
     } else {
         print("odd number")
+        print("with a reminder")
     }
 }
 
 var number = 24
+var number3 = 89
 
 evenNumber(ourNumber: number)
+onthree(threeNumber: number3)
